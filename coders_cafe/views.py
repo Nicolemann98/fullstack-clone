2a1231ed001d1dd7b82ff4a6087f61be37398822
Nicole Mann <nicolemann98@outlook.com>
2024-05-27 21:07:23 +0000

    django settings

--- coders_cafe/views.py
+++ coders_cafe/views.py
@@ -3,7 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-   if request.method == "GET":
-       return HttpResponse("This was a GET request")
-   elif request.method == "POST":
-       return HttpResponse("This was a POST request")
+    return HttpResponse("Hello world!!")
