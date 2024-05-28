447867eb900d19566b385ed05d5b895348ee3558
Nicole Mann <nicolemann98@outlook.com>
2024-05-28 09:26:50 +0000

    Add booking table

--- coders_cafe/models.py
+++ coders_cafe/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Booking(models.Model):
+    user_id = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )
+    start_time = models.DateTimeField()
+    num_seats = models.IntegerField()
+    
