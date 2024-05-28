77146323f5d860c24c705975e926e5fd74d4f7c4
Nicole Mann <nicolemann98@outlook.com>
2024-05-28 10:13:01 +0000

    add view for all bookings

--- coders_cafe/views.py
+++ coders_cafe/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Booking
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello world!!")
+class BookingList(generic.ListView):
+    model = Booking
--- my_project/urls.py
+++ my_project/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from coders_cafe import views as index_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path("", include("coders_cafe.urls"), name="cosers_cafe-urls"),
     path('admin/', admin.site.urls),
 ]
