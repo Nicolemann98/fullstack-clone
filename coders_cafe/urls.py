8964db2477736d53b5bea0ed22aff9e8e671d315
Nicole Mann <nicolemann98@outlook.com>
2024-05-31 16:35:17 +0000

    create accounts page

--- coders_cafe/urls.py
+++ coders_cafe/urls.py
@@ -5,7 +5,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='coders_cafe/index.html'), name='index'),
     path('menu/', TemplateView.as_view(template_name='coders_cafe/menu.html'), name='menu'),
-    path('cancel/', TemplateView.as_view(template_name='coders_cafe/cancel.html'), name='cancel'),
+    path('account/', TemplateView.as_view(template_name='coders_cafe/account.html'), name='account'),
     path('bookings/', views.BookingList.as_view(), name='bookings'),
     path('manage-booking/<int:booking>', views.manage_booking, name='manage_booking')
 ]
