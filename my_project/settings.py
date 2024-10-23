2d6bd6390b63fd42198fe33905205816b230f417
Nicolemann98 <144236128+Nicolemann98@users.noreply.github.com>
2024-10-23 18:21:49 +0100

    Remove secret key

--- my_project/settings.py
+++ my_project/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gt#gts*x+hty$b9umgxg1j6p&*#*v)w45$-w2h)n@ngkwple$#'
+SECRET_KEY = os.environ.get("DATABASE_URL")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
